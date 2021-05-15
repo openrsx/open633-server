7932c744a87f2eef7f03d97d4157daa442cbb448
CSS-Lletya <51089431+CSS-Lletya@users.noreply.github.com>
2021-05-15 12:04:50 -0400

    Controller Handler Loading improvements
    The Controller handler will now automatically get the Classes within the package instead of declaring classes as seen before
    (Not more adding lines to this class anymore).

--- #633 - Server/src/com/rs/game/player/controllers/ControlerHandler.java
+++ #633 - Server/src/com/rs/game/player/controllers/ControlerHandler.java
@@ -2,48 +2,34 @@ package com.rs.game.player.controllers;
 
 import java.util.HashMap;
 
-import com.rs.game.minigames.WarriorsGuild;
-import com.rs.game.minigames.duel.DuelArena;
 import com.rs.utils.Logger;
+import com.rs.utils.Utils;
 
 public class ControlerHandler {
 
-	private static final HashMap<Object, Class<? extends Controller>> handledControlers = new HashMap<Object, Class<? extends Controller>>();
+	private static final HashMap<Object, Class<Controller>> handledControlers = new HashMap<Object, Class<Controller>>();
 
 	@SuppressWarnings("unchecked")
 	public static final void init() {
 		try {
-			Class<Controller> value1 = (Class<Controller>) Class.forName(Wilderness.class.getCanonicalName());
-			handledControlers.put("Wilderness", value1);
-			Class<Controller> value4 = (Class<Controller>) Class.forName(GodWars.class.getCanonicalName());
-			handledControlers.put("GodWars", value4);
-			Class<Controller> value5 = (Class<Controller>) Class.forName(ZGDControler.class.getCanonicalName());
-			handledControlers.put("ZGDControler", value5);
-			Class<Controller> value9 = (Class<Controller>) Class.forName(DuelArena.class.getCanonicalName());
-			handledControlers.put("DuelArena", value9);
-			Class<Controller> value10 = (Class<Controller>) Class.forName(DuelControler.class.getCanonicalName());
-			handledControlers.put("DuelControler", value10);
-			Class<Controller> value15 = (Class<Controller>) Class.forName(JailControler.class.getCanonicalName());
-			handledControlers.put("JailControler", value15);
-			handledControlers.put("WarriorsGuild",
-					(Class<Controller>) Class.forName(WarriorsGuild.class.getCanonicalName()));
-			// handledControlers.put("SlaughterFieldsControler",
-			// (Class<Controller>)
-			// Class.forName(SlaughterFieldsControler.class.getCanonicalName()));
+			
+			Class<Controller>[] regular = Utils.getClasses("com.rs.game.player.controllers");
+			for (Class<Controller> c : regular) {
+				if (c.isAnonymousClass()) // next
+					continue;
+				handledControlers.put(c.getSimpleName(), c);
+			}
+			
 		} catch (Throwable e) {
 			Logger.handle(e);
 		}
 	}
 
-	public static final void reload() {
-		handledControlers.clear();
-		init();
-	}
-
+	@SuppressWarnings("deprecation")
 	public static final Controller getControler(Object key) {
 		if (key instanceof Controller)
 			return (Controller) key;
-		Class<? extends Controller> classC = handledControlers.get(key);
+		Class<Controller> classC = handledControlers.get(key);
 		if (classC == null)
 			return null;
 		try {
