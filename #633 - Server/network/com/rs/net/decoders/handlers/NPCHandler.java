010dd122b9a4ee9fbdba48904eb605c4867d16bd
CSS-Lletya <51089431+CSS-Lletya@users.noreply.github.com>
2021-05-21 14:26:57 -0400

    NPC Interact packet added
    Test on npc interact one; need more npc spawns with more options to further test in advance.

--- #633 - Server/network/com/rs/net/decoders/handlers/NPCHandler.java
+++ #633 - Server/network/com/rs/net/decoders/handlers/NPCHandler.java
@@ -12,72 +12,49 @@ import com.rs.utils.NPCExamines;
 public class NPCHandler {
 
 	public static void handleExamine(final Player player, InputStream stream) {
-		int npcIndex = stream.readUnsignedShort128();
-		boolean forceRun = stream.read128Byte() == 1;
-		if (forceRun)
-			player.setRun(forceRun);
+		int npcIndex = stream.readUnsignedShort();
 		final NPC npc = World.getNPCs().get(npcIndex);
-		if (npc == null || npc.hasFinished() || !player.getMapRegionsIds().contains(npc.getRegionId()))
+		if (npc == null || npc.isCantInteract() || npc.isDead() || npc.hasFinished()
+				|| !player.getMapRegionsIds().contains(npc.getRegionId()) || player.isLocked())
 			return;
-		player.getPackets().sendNPCMessage(0, 15263739, npc, NPCExamines.getExamine(npc));
-		player.getPackets().sendResetMinimapFlag();
-		if (Settings.DEBUG)
-			Logger.log("NPCHandler", "examined npc: " + npcIndex + ", " + npc.getId());
+		System.out.println("?");
+		System.out.println(npc.getName());
 	}
 
 	public static void handleOption1(final Player player, final InputStream stream) {
-		int npcIndex = stream.readUnsignedShort128();
-		boolean forceRun = stream.read128Byte() == 1;
+		int npcIndex = stream.readUnsignedShort() << 32;
 		final NPC npc = World.getNPCs().get(npcIndex);
 		if (npc == null || npc.isCantInteract() || npc.isDead() || npc.hasFinished()
 				|| !player.getMapRegionsIds().contains(npc.getRegionId()) || player.isLocked())
 			return;
-		player.stopAll();
-		if (forceRun)
-			player.setRun(forceRun);
+		System.out.println(npc.getName());
 	}
 
 	public static void handleOption2(final Player player, InputStream stream) {
-		int npcIndex = stream.readUnsignedShort128();
-		boolean forceRun = stream.read128Byte() == 1;
+		int npcIndex = stream.readUnsignedShort() << 32;
 		final NPC npc = World.getNPCs().get(npcIndex);
 		if (npc == null || npc.isCantInteract() || npc.isDead() || npc.hasFinished()
 				|| !player.getMapRegionsIds().contains(npc.getRegionId()) || player.isLocked())
 			return;
-		player.stopAll();
-		if (forceRun)
-			player.setRun(forceRun);
+		System.out.println(npc.getName());
 	}
 
 	public static void handleOption3(final Player player, InputStream stream) {
-		int npcIndex = stream.readUnsignedShort128();
-		boolean forceRun = stream.read128Byte() == 1;
+		int npcIndex = stream.readUnsignedShort() << 32;
 		final NPC npc = World.getNPCs().get(npcIndex);
 		if (npc == null || npc.isCantInteract() || npc.isDead() || npc.hasFinished()
 				|| !player.getMapRegionsIds().contains(npc.getRegionId()) || player.isLocked())
 			return;
-		player.stopAll(false);
-		if (forceRun)
-			player.setRun(forceRun);
-		if (Settings.DEBUG)
-			System.out.println("cliked 3 at npc id : " + npc.getId() + ", " + npc.getX() + ", " + npc.getY() + ", "
-					+ npc.getPlane());
+		System.out.println(npc.getName());
 	}
 
 	public static void handleOption4(final Player player, InputStream stream) {
-		int npcIndex = stream.readUnsignedShort128();
-		boolean forceRun = stream.read128Byte() == 1;
+		int npcIndex = stream.readUnsignedShort() << 32;
 		final NPC npc = World.getNPCs().get(npcIndex);
 		if (npc == null || npc.isCantInteract() || npc.isDead() || npc.hasFinished()
 				|| !player.getMapRegionsIds().contains(npc.getRegionId()) || player.isLocked())
 			return;
-		player.stopAll(false);
-		if (forceRun)
-			player.setRun(forceRun);
-	
-		if (Settings.DEBUG)
-			System.out.println("cliked 4 at npc id : " + npc.getId() + ", " + npc.getX() + ", " + npc.getY() + ", "
-					+ npc.getPlane());
+		System.out.println(npc.getName());
 
 	}
 
