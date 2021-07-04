2d3b63b0814d72b0c31212e25a0b458071084ca4
CSS-Lletya <51089431+CSS-Lletya@users.noreply.github.com>
2021-07-04 12:32:17 -0400

    Player Customizing interface added
    Added back support fort Player design customizing. Full appearance modifiable.

--- #633 - Server/plugins/com/rs/plugin/impl/commands/TestCommand.java
+++ #633 - Server/plugins/com/rs/plugin/impl/commands/TestCommand.java
@@ -2,21 +2,21 @@ package com.rs.plugin.impl.commands;
 
 import com.rs.game.player.Player;
 import com.rs.game.player.Rights;
-import com.rs.game.player.controller.impl.WildernessController;
+import com.rs.game.player.content.PlayerLook;
 import com.rs.plugin.listener.Command;
 import com.rs.plugin.wrapper.CommandSignature;
 
 /**
- * This is just a dummy command to re-use
- * for whatever testing needed.
+ * This is just a dummy command to re-use for whatever testing needed.
+ * 
  * @author Dennis
  *
  */
-@CommandSignature(alias = {"test"}, rights = {Rights.PLAYER}, syntax = "Test a Command")
+@CommandSignature(alias = { "test" }, rights = { Rights.PLAYER }, syntax = "Test a Command")
 public final class TestCommand implements Command {
-	
+
 	@Override
 	public void execute(Player player, String[] cmd, String command) throws Exception {
-		new WildernessController().start(player);
+		PlayerLook.openCharacterCustomizing(player);
 	}
 }
